a84bd41c4eab82e0eebe7fd5769594114c3658dd
alexandrakel <83000445+alexandrakel@users.noreply.github.com>
2021-08-01 00:58:42 +0100

    first commit

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,6 @@
+<?php return array (
+  'about' => 'App\\Http\\Livewire\\About',
+  'contact' => 'App\\Http\\Livewire\\Contact',
+  'home' => 'App\\Http\\Livewire\\Home',
+  'roadmap' => 'App\\Http\\Livewire\\Roadmap',
+);
---
+++ storage/framework/views/b316d9df90cd3d967cc91507a7ade686184c1e9b.php
@@ -0,0 +1,21 @@
+<!-- Menu -->
+<li><a href="<?php echo e(route('home')); ?>" title="">Home</a></li>
+<li><a href="<?php echo e(route('home')); ?>" title="">uniswap</a></li>
+<li><a href="<?php echo e(route('home')); ?>" title="">farm spickles</a></li>
+<li><a href="<?php echo e(route('home')); ?>" title="">staking</a></li>
+<li><a href="<?php echo e(route('about')); ?>" title="">about us</a>
+<li><a href="<?php echo e(route('roadmap')); ?>" title="">road map</a>
+<li><a href="<?php echo e(route('roadmap')); ?>#how-it-works" title="">how it</a>
+<li><a href="<?php echo e(route('how.it.works')); ?>" title="">how it works</a>
+<li><a href="<?php echo e(route('how.it.works')); ?>" title="">markets</a>
+<li><a href="<?php echo e(route('how.it.works')); ?>" title="">buy</a>
+<li><a href="<?php echo e(route('contact')); ?>" title="">Contact us</a></li>
+<li><a href="team.html" title="">0x1d36...c4da</a>
+    <ul>
+        <li class="dropdown_hover"><a href="team.html" title=""> team</a></li>
+        <li class="dropdown_hover"><a href="team_details.html" title="">team details</a>
+        </li>
+    </ul>
+</li>
+
+<?php /**PATH C:\xampp\htdocs\websites\blockchain\Ketnology\solar-livewire\sunlar\resources\views/layouts/menu.blade.php ENDPATH**/ ?>
